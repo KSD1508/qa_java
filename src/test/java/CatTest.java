c5df59e67227f4e495df2a54f9fe3823a83aedaf
s.kompaneytseva <s.kompaneytseva@qiwi.com>
2023-04-10 14:41:15 +0300

    [NF] adding new tests

--- src/test/java/CatTest.java
+++ src/test/java/CatTest.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class CatTest {
+import com.example.Cat;
+import com.example.Feline;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class CatTest {
+    @Mock
+    Feline feline;
+
+    @Test
+    public void testGetSound(){
+        Cat cat = new Cat(feline);
+
+        assertEquals("Мяу", cat.getSound());
+    }
+
+    @Test
+    public void testGetFood() throws Exception {
+        when(feline.eatMeat()).thenReturn(List.of("Животные", "Птицы", "Рыба"));
+
+        Cat cat = new Cat(feline);
+
+        assertEquals(List.of("Животные", "Птицы", "Рыба"),cat.getFood());
+
+        reset(feline);
+    }
 }
--- src/test/java/LionTest.java
+++ src/test/java/LionTest.java
@@ -1,2 +1,62 @@
-package PACKAGE_NAME;public class LionTest {
+import com.example.Feline;
+import com.example.Lion;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class LionTest {
+    @Mock
+    Feline feline;
+
+    @ParameterizedTest
+    @CsvSource({
+            "Самец, true",
+            "Самка, false"
+    })
+    void shouldSuccessCreateLion(String sex, boolean hasMane) throws Exception {
+        Lion lion = new Lion(sex, feline);
+
+        assertEquals(hasMane, lion.doesHaveMane());
+
+    }
+
+    @Test
+    void shouldFailedCreateLionByUnknownSex() {
+        String unknownSex = "unknownSex";
+        Throwable error = assertThrows(Exception.class, () -> {
+            new Lion(unknownSex, feline);
+        });
+
+        assertEquals("Используйте допустимые значения пола животного - самей или самка", error.getMessage());
+    }
+
+    @Test
+    void testGetKittens() throws Exception {
+        when(feline.getKittens()).thenReturn(1);
+
+        Lion lion = new Lion("Самец", feline);
+
+        assertEquals(1, lion.getKittens());
+    }
+
+    @Test
+    void testGetFoods() throws Exception {
+        when(feline.getFood("Хищник")).thenReturn(List.of("Животные", "Птицы", "Рыба"));
+
+        Lion lion = new Lion("Самец", feline);
+
+        assertEquals(List.of("Животные", "Птицы", "Рыба"), lion.getFood());
+    }
 }
